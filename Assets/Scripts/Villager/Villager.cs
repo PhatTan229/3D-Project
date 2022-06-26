9bdfa3fbc5866304ece1d5f16d0dc2e2aa85d3ae
Thach Trinh <tqbaothach@gmail.com>
2022-06-26 18:09:52 +0700

    Fix bug villager

--- Assets/Scripts/Villager/Villager.cs
+++ Assets/Scripts/Villager/Villager.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public enum VillagerWork
 {
+    None, Talking,
     Building1, Building2, Building3,
     Cooking1, Cooking2,
     Farming1, Farming2, Farming3,
-    Chopping1, Chopping2, Sawing1, Sawing2,
+    Chopping1, Chopping2, Sawing,
     PickaxeMining, ShovelWorking,
     Fishing
 }
