e1036cf51bc92e09b0278fbd696844455dd07129
Thach Trinh <tqbaothach@gmail.com>
2022-06-27 20:35:52 +0700

    Minimap, stat bar

--- Assets/Scripts/PlayerSpawner.cs
+++ Assets/Scripts/PlayerSpawner.cs
@@ -8,11 +8,15 @@ public class PlayerSpawner : MonoBehaviour
     public PlayerManagement player;
     public CinemachineVirtualCamera aimingCamera;
     public ThirdPersonCamera thirdPersonCamera;
+    public MinimapCamera minimapCamera;
+    public MinimapCompass compass;
     private void Start()
     {
         PlayerManagement myPlayer = Instantiate(player, transform.position, Quaternion.identity);
         aimingCamera.Follow = myPlayer.transform;
         thirdPersonCamera.target = myPlayer.transform;
+        minimapCamera.target = myPlayer.transform;
+        compass.thirdPersonCamera = thirdPersonCamera.transform;
         myPlayer.movement.thirdPersonCamera = thirdPersonCamera.transform;
         myPlayer.weapon.thirdPersonCamera = thirdPersonCamera.gameObject;
         myPlayer.weapon.aimingCamera = aimingCamera.gameObject;
