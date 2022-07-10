6cb1e9531be658dcc958dee1325bd739e859601a
Thach Trinh <tqbaothach@gmail.com>
2022-07-10 11:29:39 +0700

    playmaker & mage animation

--- Assets/Scripts/Enemy/ZombieDog/ZombieDog.cs
+++ Assets/Scripts/Enemy/ZombieDog/ZombieDog.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ZombieDog : MonoBehaviour
+public class ZombieDog : EnemyManagement
 {
-    // Start is called before the first frame update
-    void Start()
+    public PlayMakerFSM fsm;
+    private CharacterManagement target;
+    protected override void RegisterEvent() { }
+    public void FindTarget()
     {
-        
+        target = PopulationManager.Instance.GetRandomTarget(animator.transform.position, Side.Ally);
+        if (target != null) fsm.SendEvent("Chase");
+    }
+    public void Action1()
+    {
+        Debug.Log("Action 1");
+        fsm.SendEvent("Chase");
+    }
+    public void Action2()
+    {
+        Debug.Log("Action 2");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
