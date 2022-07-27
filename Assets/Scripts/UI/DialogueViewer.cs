efb39d846ec656cd15e62652df6cc95120d3c39a
Thach Trinh <tqbaothach@gmail.com>
2022-07-27 17:55:38 +0700

    push

--- Assets/Scripts/UI/DialogueViewer.cs
+++ Assets/Scripts/UI/DialogueViewer.cs
@@ -5,12 +5,20 @@ using TMPro;
 
 public class DialogueViewer : MonoBehaviour
 {
-    
+    private static string nameKey = "@name";
     public TMP_Text text;
     public void Show() => gameObject.SetActive(true);
     public void Hide() => gameObject.SetActive(false);
-    public void ShowDialogue(string speaker, string content)
+    public void ShowDialogue(string speaker, string speech)
     {
-        text.text = speaker + " : " + content;
+        Debug.Log("ShowDialogue1");
+        string newSpeech = speech;
+        if (speech.Contains(nameKey))
+        {
+            Debug.Log("ShowDialogue2");
+            if (DatabaseController.Instance)
+                newSpeech = speech.Replace(nameKey, DatabaseController.Instance.data.name);
+        }
+        text.text = speaker + " : " + newSpeech;
     }
 }
